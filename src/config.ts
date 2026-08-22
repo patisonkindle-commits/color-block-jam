9cd5974754a2fea78f3fa6bc1c26f727f9b0c8d9
Patison Palee <patison@kindle.com>
2026-08-22 18:27:40 +0700

    fix: use Phaser.CANVAS instead of Phaser.AUTO to fix mobile Android rendering issue

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 
 export const GAME_CONFIG: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     width: 720,
     height: 1280,
     backgroundColor: '#1a1a2e',
